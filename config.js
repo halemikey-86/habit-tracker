3638aa1d7d490c13e94a344061de9a7eb844f08f
halemikey-86 <halemikey@icloud.com>
2026-06-22 09:06:12 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // Project Settings -> API -> Project URL / Project API keys -> anon public
 window.HQ_CONFIG = {
   SUPABASE_URL: "https://uxaktbbddjjbgldvomga.supabase.co",
-  SUPABASE_ANON_KEY: "YOUR-ANON-PUBLIC-KEY"
+  SUPABASE_ANON_KEY: "sb_publishable_v-3sLkMmkfl1iQiGw3eqGw_5k1Jd5EJ"
 };
